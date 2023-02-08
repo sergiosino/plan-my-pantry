776f1d4b9e76041085271133ee1fb53805ca564a
Sergio Iglesias Traviesa <sergiosino96@hotmail.com>
2023-02-08 22:29:07 +0100

    Separated grocery item component

---
+++ views/GroceryListView/GroceryItem.js
@@ -0,0 +1,95 @@
+import Checkbox from 'expo-checkbox'
+import { StyleSheet, Text, TextInput, TouchableNativeFeedback, View } from 'react-native'
+import Ionicons from '@expo/vector-icons/Ionicons'
+import { useState } from 'react'
+
+const CHECKED_COLOR = 'gray'
+const GROCERY_ITEM_HEIGHT = 44
+
+export default function GroceryItem (props) {
+  const { defaultChecked, defaultText, handleDeleteItem, isItemToFocus, onItemChange } = props
+
+  const [isChecked, setIsChecked] = useState(defaultChecked)
+  const [text, setText] = useState(defaultText)
+  const [showDeleteIcon, setShowDeleteIcon] = useState(false)
+
+  const handleTextFocus = () => {
+    setShowDeleteIcon(true)
+  }
+
+  const handleTextFocusEnd = () => {
+    console.log('hola')
+    setShowDeleteIcon(false)
+    onItemChange(isChecked, text)
+  }
+
+  const handleCheckboxChange = (value) => {
+    onItemChange(value)
+    setIsChecked(value, text)
+  }
+
+  return (
+    <View
+      style={styles.container}
+    >
+      <Checkbox
+        value={isChecked}
+        onValueChange={handleCheckboxChange}
+        color={isChecked ? CHECKED_COLOR : undefined}
+        style={styles.checkbox}
+      />
+      {isChecked
+        ? (
+          <Text style={styles.text}>
+            {text}
+          </Text>
+          )
+        : (
+          <TextInput
+            autoFocus={isItemToFocus}
+            style={styles.textInput}
+            value={text}
+            onChangeText={setText}
+            onFocus={handleTextFocus}
+            onEndEditing={handleTextFocusEnd}
+          />
+          )}
+      {showDeleteIcon && !isChecked && (
+        <TouchableNativeFeedback
+          onPress={handleDeleteItem}
+          background={TouchableNativeFeedback.Ripple(CHECKED_COLOR, true)}
+        >
+          <View style={styles.iconContainer}>
+            <Ionicons name='close' size={25} />
+          </View>
+        </TouchableNativeFeedback>
+      )}
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    paddingVertical: 8,
+    height: GROCERY_ITEM_HEIGHT
+  },
+  checkbox: {
+    marginRight: 16,
+    color: 'red'
+  },
+  text: {
+    flex: 1,
+    textDecorationLine: 'line-through',
+    color: 'gray',
+    marginVertical: 4.1
+  },
+  textInput: {
+    flex: 1
+  },
+  iconContainer: {
+    height: 25,
+    width: 25
+  }
+})
