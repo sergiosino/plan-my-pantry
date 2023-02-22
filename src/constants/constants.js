2f97edafe74d0a90766f0f600ace81ad9c827c4e
Sergio Iglesias Traviesa <sergiosino96@hotmail.com>
2023-02-22 20:14:47 +0100

    Added ingredient list const for async storage

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -2,5 +2,6 @@ export const GROCERY_ITEM_HEIGHT = 44
 export const INGREDIENT_HEIGHT = 40
 
 export const ASYNC_STORAGE_KEYS = {
-  GROCERY_LIST: 'grocery-list'
+  GROCERY_LIST: 'grocery-list',
+  INGREDIENTS_LIST: 'ingredients-list'
 }
