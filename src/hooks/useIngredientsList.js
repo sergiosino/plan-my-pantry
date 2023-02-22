041140e98223df939f44dbe22fc59e14a15cc44b
Sergio Iglesias Traviesa <sergiosino96@hotmail.com>
2023-02-22 22:59:38 +0100

    Added hook for ingredients list

---
+++ src/hooks/useIngredientsList.js
@@ -0,0 +1,90 @@
+import AsyncStorage from '@react-native-async-storage/async-storage'
+import { useEffect, useState } from 'react'
+import { ASYNC_STORAGE_KEYS } from '../constants/constants'
+import uuid from 'react-native-uuid'
+
+export function useIngredientsList (props) {
+  const { itemIndexToFocus } = props
+
+  const [selectedList, setSelectedList] = useState([])
+  const [ingredientsList, setIngredientsList] = useState([])
+
+  const updateIngredientsList = async (newIngredientsList) => {
+    const jsonValue = JSON.stringify(newIngredientsList)
+    AsyncStorage.setItem(ASYNC_STORAGE_KEYS.INGREDIENTS_LIST, jsonValue)
+    setIngredientsList(newIngredientsList)
+  }
+
+  const handleAddIngredient = () => {
+    const newIngredient = { id: uuid.v4(), text: '' }
+    const newIngredientsList = [...ingredientsList]
+    newIngredientsList.push(newIngredient)
+    itemIndexToFocus.current = newIngredientsList.length - 1
+    updateIngredientsList(newIngredientsList)
+  }
+
+  const handleSelectIngredient = (id) => {
+    const newSelectedList = [...selectedList]
+    newSelectedList.push(id)
+    setSelectedList(newSelectedList)
+  }
+
+  const handleUnselectIngredient = (id) => {
+    const ingredientIndex = selectedList.findIndex(ingredientId => ingredientId === id)
+
+    if (ingredientIndex >= 0) {
+      const newSelectedList = [...selectedList]
+      newSelectedList.splice(ingredientIndex, 1)
+      setSelectedList(newSelectedList)
+    }
+  }
+
+  const handleUnselectAllIngredients = () => {
+    setSelectedList([])
+  }
+
+  const handleIngredientChange = (id, text) => {
+    const ingredientIndex = ingredientsList.findIndex(ingredient => ingredient.id === id)
+    const ingredient = ingredientsList[ingredientIndex]
+
+    if (ingredient.text !== text) {
+      const newIngredientsList = [...ingredientsList]
+      newIngredientsList[ingredientIndex] = { id, text }
+      updateIngredientsList(newIngredientsList)
+    }
+  }
+
+  const handleDeleteIngredient = (id) => {
+    const newIngredientsList = ingredientsList.filter(ingredient => ingredient.id !== id)
+    updateIngredientsList(newIngredientsList)
+    handleUnselectIngredient(id)
+  }
+
+  const handleDeleteSelectedIngredients = () => {
+    const newIngredientsList = ingredientsList.filter(ingredient => !selectedList.includes(ingredient.id))
+    updateIngredientsList(newIngredientsList)
+    setSelectedList([])
+  }
+
+  const getStorageIngredientsList = async () => {
+    let storageIngredientsList = await AsyncStorage.getItem(ASYNC_STORAGE_KEYS.INGREDIENTS_LIST)
+    storageIngredientsList = storageIngredientsList != null ? JSON.parse(storageIngredientsList) : null
+    if (storageIngredientsList) { setIngredientsList(storageIngredientsList) }
+  }
+
+  useEffect(() => {
+    getStorageIngredientsList()
+  }, [])
+
+  return {
+    ingredientsList,
+    selectedList,
+    handleAddIngredient,
+    handleSelectIngredient,
+    handleUnselectIngredient,
+    handleUnselectAllIngredients,
+    handleIngredientChange,
+    handleDeleteIngredient,
+    handleDeleteSelectedIngredients
+  }
+}
--- src/views/RecipesView/IngredientsList.js
+++ src/views/RecipesView/IngredientsList.js
@@ -1,82 +1,26 @@
-import AsyncStorage from '@react-native-async-storage/async-storage'
-import { useEffect, useRef, useState } from 'react'
+import { useEffect, useRef } from 'react'
 import { FlatList, View } from 'react-native'
-import uuid from 'react-native-uuid'
 
 import AddButton from '../../components/AddButton'
-import { ASYNC_STORAGE_KEYS, INGREDIENT_HEIGHT } from '../../constants/constants'
+import { INGREDIENT_HEIGHT } from '../../constants/constants'
 import IngredientItem from './IngredientItem'
 import IngredientsHeader from './IngredientsHeader'
+import { useIngredientsList } from '../../hooks/useIngredientsList'
 
 export default function IngredientsList () {
-  const [selectedList, setSelectedList] = useState([])
-  const [ingredientsList, setIngredientsList] = useState([])
-
   const itemIndexToFocus = useRef(null)
   const refFlatList = useRef(null)
-
-  const updateIngredientsList = async (newIngredientsList) => {
-    const jsonValue = JSON.stringify(newIngredientsList)
-    AsyncStorage.setItem(ASYNC_STORAGE_KEYS.INGREDIENTS_LIST, jsonValue)
-    setIngredientsList(newIngredientsList)
-  }
-
-  const handleAddIngredient = () => {
-    const newIngredient = { id: uuid.v4(), text: '' }
-    const newIngredientsList = [...ingredientsList]
-    newIngredientsList.push(newIngredient)
-    itemIndexToFocus.current = newIngredientsList.length - 1
-    updateIngredientsList(newIngredientsList)
-  }
-
-  const handleSelectIngredient = (id) => {
-    const newSelectedList = [...selectedList]
-    newSelectedList.push(id)
-    setSelectedList(newSelectedList)
-  }
-
-  const handleUnselectIngredient = (id) => {
-    const ingredientIndex = selectedList.findIndex(ingredientId => ingredientId === id)
-
-    if (ingredientIndex >= 0) {
-      const newSelectedList = [...selectedList]
-      newSelectedList.splice(ingredientIndex, 1)
-      setSelectedList(newSelectedList)
-    }
-  }
-
-  const handleUnselectAllIngredients = () => {
-    setSelectedList([])
-  }
-
-  const handleIngredientChange = (id, text) => {
-    const ingredientIndex = ingredientsList.findIndex(ingredient => ingredient.id === id)
-    const ingredient = ingredientsList[ingredientIndex]
-
-    if (ingredient.text !== text) {
-      const newIngredientsList = [...ingredientsList]
-      newIngredientsList[ingredientIndex] = { id, text }
-      updateIngredientsList(newIngredientsList)
-    }
-  }
-
-  const handleDeleteIngredient = (id) => {
-    const newIngredientsList = ingredientsList.filter(ingredient => ingredient.id !== id)
-    updateIngredientsList(newIngredientsList)
-    handleUnselectIngredient(id)
-  }
-
-  const handleDeleteSelectedIngredients = () => {
-    const newIngredientsList = ingredientsList.filter(ingredient => !selectedList.includes(ingredient.id))
-    updateIngredientsList(newIngredientsList)
-    setSelectedList([])
-  }
-
-  const getStorageIngredientsList = async () => {
-    let storageIngredientsList = await AsyncStorage.getItem(ASYNC_STORAGE_KEYS.INGREDIENTS_LIST)
-    storageIngredientsList = storageIngredientsList != null ? JSON.parse(storageIngredientsList) : null
-    if (storageIngredientsList) { setIngredientsList(storageIngredientsList) }
-  }
+  const {
+    ingredientsList,
+    selectedList,
+    handleAddIngredient,
+    handleSelectIngredient,
+    handleUnselectIngredient,
+    handleUnselectAllIngredients,
+    handleIngredientChange,
+    handleDeleteIngredient,
+    handleDeleteSelectedIngredients
+  } = useIngredientsList({ itemIndexToFocus })
 
   // When a new item is added it scrolls to its location
   useEffect(() => {
@@ -88,10 +32,6 @@ export default function IngredientsList () {
       })
     }
   }, [ingredientsList])
-
-  useEffect(() => {
-    getStorageIngredientsList()
-  }, [])
 
   const isSelectedListEmpty = selectedList.length === 0
 
