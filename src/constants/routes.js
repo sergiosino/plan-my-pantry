da98c22e0c2931286ef1b6e0ceb9e2c92077904c
Sergio Iglesias Traviesa <sergiosino96@hotmail.com>
2023-02-24 21:52:02 +0100

    Added routes names to constants

---
+++ src/constants/routes.js
@@ -0,0 +1,5 @@
+export const ROUTE_NAME_RECIPES = 'Recipes'
+export const ROUTE_NAME_INGREDIENTS = 'Ingredients'
+export const ROUTE_NAME_FOODS = 'Foods'
+export const ROUTE_NAME_WEEK_MENU = 'Week menu'
+export const ROUTE_NAME_GROCERY_LIST = 'Grocery list'
