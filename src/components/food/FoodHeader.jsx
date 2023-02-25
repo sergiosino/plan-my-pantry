0b7ee3e5888321452880bd9900580c4d46b6ada0
Sergio Iglesias Traviesa <sergiosino96@hotmail.com>
2023-02-25 12:40:41 +0100

    Added icons to foods header buttons

--- src/components/food/FoodHeader.jsx
+++ src/components/food/FoodHeader.jsx
@@ -4,11 +4,24 @@ import { headerStyles } from '../../styles/headerStyles'
 import { ROUTE_NAME_RECIPES, ROUTE_NAME_INGREDIENTS } from '../../constants/routes'
 import FoodHeaderItem from './FoodHeaderItem'
 
+const FOOD_HEADER_ITEMS = [
+  {
+    routeName: ROUTE_NAME_RECIPES,
+    iconName: 'book-outline'
+  },
+  {
+    routeName: ROUTE_NAME_INGREDIENTS,
+    iconName: 'basket-outline'
+  }
+]
+
 export default function FoodHeader () {
   return (
     <View style={[headerStyles.headerContainer]}>
-      <FoodHeaderItem routeName={ROUTE_NAME_RECIPES} />
-      <FoodHeaderItem routeName={ROUTE_NAME_INGREDIENTS} />
+      {FOOD_HEADER_ITEMS.map(item => {
+        const { routeName, iconName } = item
+        return <FoodHeaderItem key={routeName} routeName={routeName} iconName={iconName} />
+      })}
     </View>
   )
 }
--- src/components/food/FoodHeaderItem.jsx
+++ src/components/food/FoodHeaderItem.jsx
@@ -1,38 +1,46 @@
 import { useNavigation, useNavigationState } from '@react-navigation/native'
 import { Pressable, StyleSheet, Text, View } from 'react-native'
+import Ionicons from '@expo/vector-icons/Ionicons'
 
 import { headerStyles } from '../../styles/headerStyles'
 
 export default function FoodHeaderItem (props) {
-  const { routeName } = props
+  const { routeName, iconName } = props
 
   const navigation = useNavigation()
   const actualRouteName = useNavigationState((state) => state.routes[state.index].name)
 
-  const getPressableStyles = (pressed, screenName) => {
-    if (actualRouteName === screenName) { return styles.textButtonSelected }
+  const getPressableStyles = (pressed) => {
+    if (actualRouteName === routeName) { return styles.textButtonSelected }
     if (pressed) { return styles.textButtonPress }
     return styles.textButtonDefault
   }
 
-  const getTextStyles = (pressed, screenName) => {
-    if (actualRouteName === screenName || pressed) { return styles.textPressOrSelected }
+  const getTextStyles = (pressed) => {
+    if (actualRouteName === routeName || pressed) { return styles.textPressOrSelected }
     return styles.textDefault
+  }
+
+  const getIcon = () => {
+    if (actualRouteName === routeName) { return <Ionicons name={iconName} size={20} style={styles.icon} /> }
   }
 
   return (
     <View style={headerStyles.headerItem}>
       <Pressable
         style={({ pressed }) => [
-          getPressableStyles(pressed, routeName),
+          getPressableStyles(pressed),
           styles.textButton
         ]}
         onPress={() => navigation.navigate(routeName)}
       >
         {({ pressed }) => (
-          <Text style={getTextStyles(pressed, routeName)}>
-            {routeName}
-          </Text>
+          <View style={styles.content}>
+            {getIcon()}
+            <Text style={getTextStyles(pressed)}>
+              {routeName}
+            </Text>
+          </View>
         )}
       </Pressable>
     </View>
@@ -50,13 +58,21 @@ const styles = StyleSheet.create({
     backgroundColor: 'gray'
   },
   textButton: {
-    padding: 6,
+    paddingVertical: 6,
+    paddingHorizontal: 10,
     borderRadius: 4
   },
   textDefault: {
     color: 'black'
   },
   textPressOrSelected: {
+    color: 'white'
+  },
+  content: {
+    flexDirection: 'row'
+  },
+  icon: {
+    marginRight: 5,
     color: 'white'
   }
 })
