7456195a7d2031ff7f99656360e5391c8b811c98
Sergio Iglesias Traviesa <sergiosino96@hotmail.com>
2023-03-19 19:47:51 +0100

    New routes and constants

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -4,15 +4,6 @@ export const INGREDIENT_HEIGHT = 36
 export const ASYNC_STORAGE_KEYS = {
   GROCERY_LIST: 'grocery-list',
   INGREDIENTS_LIST: 'ingredients-list',
-  RECIPES_LIST: 'recipes-list'
+  RECIPES_LIST: 'recipes-list',
+  WEEK_MENU: 'week-menu'
 }
-
-export const WEEK_DAYS = [
-  { id: 1, name: 'Monday' },
-  { id: 2, name: 'Tuesday' },
-  { id: 3, name: 'Wednesday' },
-  { id: 4, name: 'Thursday' },
-  { id: 5, name: 'Friday' },
-  { id: 6, name: 'Saturday' },
-  { id: 7, name: 'Sunday' }
-]
