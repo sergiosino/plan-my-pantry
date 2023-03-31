bedd851257abfeae291e0b20996f40a551939e80
Sergio Iglesias Traviesa <sergiosino96@hotmail.com>
2023-03-31 17:08:35 +0200

    New isNullOrWhiteSpace util

---
+++ src/utils/index.js
@@ -0,0 +1,11 @@
+import { areObjectsEqual } from './areObjectsEqual'
+import { capitalizeString } from './capitalizeString'
+import { confirmationAlert } from './confirmationAlert'
+import { isNullOrWhiteSpaced } from './isNullOrWhiteSpaced'
+
+export {
+  areObjectsEqual,
+  capitalizeString,
+  confirmationAlert,
+  isNullOrWhiteSpaced
+}
---
+++ src/utils/isNullOrWhiteSpace.js
@@ -0,0 +1,3 @@
+export function isNullOrWhiteSpace (text) {
+  return !text || !text.trim()
+}
