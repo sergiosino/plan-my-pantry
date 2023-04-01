1e8bd6641740eb68ed7b667f5d4e300485d6ff6b
Sergio Iglesias Traviesa <sergiosino96@hotmail.com>
2023-04-01 21:57:00 +0200

    Stack routes moved to constant

--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -8,21 +8,36 @@ import { useTheme } from '@react-navigation/native'
 
 const Stack = createNativeStackNavigator()
 
+const STACK_ROUTES = [
+  {
+    name: ROUTE_NAME_TABS,
+    component: BottomTabsRoutes,
+    options: { headerShown: false }
+  },
+  {
+    name: ROUTE_NAME_RECIPES_EDIT,
+    component: RecipeEditView
+  },
+  {
+    name: ROUTE_NAME_DAY_MENU_EDIT,
+    component: DayMenuEditView
+  }
+]
+
 export default function Routes () {
   const { colors } = useTheme()
 
   return (
     <Stack.Navigator
       screenOptions={{
-        headerStyle: {
-          backgroundColor: colors.background
-        },
+        headerStyle: { backgroundColor: colors.background },
         headerShadowVisible: false
       }}
     >
-      <Stack.Screen name={ROUTE_NAME_TABS} component={BottomTabsRoutes} options={{ headerShown: false }} />
-      <Stack.Screen name={ROUTE_NAME_RECIPES_EDIT} component={RecipeEditView} />
-      <Stack.Screen name={ROUTE_NAME_DAY_MENU_EDIT} component={DayMenuEditView} />
+      {STACK_ROUTES.map(stackRoute => {
+        const { name, component, options } = stackRoute
+        return <Stack.Screen key={name} name={name} component={component} options={options} />
+      })}
     </Stack.Navigator>
   )
 }
