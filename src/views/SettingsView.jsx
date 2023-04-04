20da14039e93c60d8ae5e57672a797c2e7d186a6
Sergio Iglesias Traviesa <sergiosino96@hotmail.com>
2023-04-04 13:33:41 +0200

    New show welcome page option added to settings view and UI improvements

--- src/views/SettingsView.jsx
+++ src/views/SettingsView.jsx
@@ -1,15 +1,22 @@
-import { useContext, useEffect, useState } from 'react'
+import { useEffect, useState } from 'react'
 import { Image, StyleSheet, Text, View } from 'react-native'
 import Constants from 'expo-constants'
 
-import { RecipesContext } from '../contexts/RecipesContext'
 import TextInputStyled from '../components/forms/TextInputSyled'
 import Divider from '../components/Divider'
+import Checkbox from '../components/forms/Checkbox'
+import { useUserConfig } from '../hooks/useUserConfig'
+import { useRecipes } from '../hooks'
 
 export default function SettingsView () {
-  const { recipes } = useContext(RecipesContext)
-
   const [textJson, setTextJson] = useState('')
+
+  const { recipes } = useRecipes()
+  const { showInitialPage, updateShowInitialHelp } = useUserConfig()
+
+  const handleCheckboxChange = (checked) => {
+    updateShowInitialHelp(checked)
+  }
 
   useEffect(() => {
     setTextJson(JSON.stringify(recipes))
@@ -20,24 +27,44 @@ export default function SettingsView () {
       <View style={styles.imageContainer}>
         <Image source={require('../../assets/chick-settings.png')} style={{ height: 80, width: 80 }} />
       </View>
-      <Divider style={styles.divider} />
-      <TextInputStyled
-        value={textJson}
-        multiline
-        numberOfLines={10}
-      />
-      <View style={{ flex: 1, alignItems: 'flex-end', justifyContent: 'flex-end' }}>
-        <Text>Version: {Constants.manifest.version}</Text>
+      <Divider />
+      <View style={{ padding: 10, backgroundColor: 'white' }}>
+        <Text style={{ marginBottom: 5 }}>Recipes JSON</Text>
+        <TextInputStyled
+          value={textJson}
+          multiline
+          numberOfLines={10}
+        />
       </View>
+      <Divider />
+      <View style={styles.settingContainer}>
+        <Text>Show welcome page</Text>
+        <Checkbox checked={showInitialPage} onChange={handleCheckboxChange} />
+      </View>
+      <Divider />
+      <View style={styles.settingContainer}>
+        <Text>App version</Text>
+        <Text>{Constants.manifest.version}</Text>
+      </View>
+      <Divider />
     </View>
   )
 }
 
 const styles = StyleSheet.create({
   imageContainer: {
-    alignItems: 'center'
+    alignItems: 'center',
+    marginBottom: 20
   },
   divider: {
     marginVertical: 20
+  },
+  settingContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    paddingHorizontal: 10,
+    height: 50,
+    backgroundColor: 'white'
   }
 })
